c9b17cb503c380b325a9f42c0b62c7ae14366599
abhijit-ghosh <abhijit@squaregroup.com>
2023-09-14 17:24:29 +0600

    3rd commit

--- PosSystem.Repository/ProductRepository.cs
+++ PosSystem.Repository/ProductRepository.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using PosSystem.Database;
+using PosSystem.Models;
+using PosSystem.Models.UtilitiesModels;
+using PosSystem.Repository.Abstraction;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PosSystem.Repository
 {
-    public class ProductRepository : 
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
     {
+        PosSystemDbContext db;
+        public ProductRepository(PosSystemDbContext db)
+        {
+            this.db = db;
+            _db = db;
+        }
+        public void Delete(int productId)
+        {
+            var product = db.Products.Find(productId);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                Console.WriteLine("Product is Deleted");
+            }
+        }
 
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Product> SearchProduct(ProductSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
